69901ab22b93b54cd2323af3984f5571a2f68451
Zehua-Chen <zehua-chen@outlook.com>
2022-06-08 14:57:04 -0500

    [feat] Start Go Project

---
+++ api/main.go
@@ -0,0 +1,43 @@
+package main
+
+import (
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+)
+
+type Person struct {
+	ID string `uri:"id" binding:"required"`
+}
+
+type Query struct {
+	Name string `form:"name"`
+}
+
+func Compartment(context *gin.Context) {
+	var person Person
+	var query Query
+
+	e := context.BindUri(&person)
+	e = context.BindQuery(&query)
+
+	if e == nil {
+		context.JSON(http.StatusOK, gin.H{
+			"id":   person.ID,
+			"name": query.Name,
+		})
+
+		return
+	}
+
+	context.String(http.StatusInternalServerError, e.Error())
+}
+
+func main() {
+	engine := gin.Default()
+	apiV1 := engine.Group("/api/v1")
+
+	apiV1.GET("/compartment/:id", Compartment)
+
+	engine.Run()
+}
